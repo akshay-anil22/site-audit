4b2e0aca9a8aa3e72b26fdf3353c1da0e6017649
akshay-anil22 <92985589+akshay-anil22@users.noreply.github.com>
2026-03-02 22:34:00 +0530

    port added

--- site-audit-backend/SiteAudit-backend/src/server.js
+++ site-audit-backend/SiteAudit-backend/src/server.js
@@ -60,7 +60,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`
 ╔═══════════════════════════════════════════════════╗
 ║                                                   ║
